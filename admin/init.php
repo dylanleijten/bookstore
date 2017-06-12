6fb0b2be36298f6c36da59c940e184b2959df7bc
Samir <samo.dn@gmail.com>
2017-06-12 18:20:54 +0200

    Nog meer structuur geven voor het project map

---
+++ admin/init.php
@@ -0,0 +1,16 @@
+<?php
+
+// Routes
+
+$tpl 	= 'includes/templates/'; // Templates directory
+$css 	= 'layout/css/'; // Css directory
+$js  	= 'layout/js/'; // Js directory
+$class 	= 'includes/classes'; // Classes directory
+$lang 	= 'includes/languages'; // languages directory
+
+
+// Include the important files
+   
+  include $lang . 'english.php';
+  include $tpl  . 'header.php'; 
+ 
