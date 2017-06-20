e57cf59b7dfad8c835d2a47384cfc9dc8d5fef1e
dylanleijten <djamleij@avans.nl>
2017-06-20 23:22:31 +0200

    layout verbeterd & images toegevoegd

--- includes/footer.php
+++ includes/footer.php
@@ -2,33 +2,16 @@
 	    <div class="footer">
 	    	<div class="container">
 		    	<div class="row">
-		    		<div class="col-footer col-md-3 col-xs-6">
-		    			<h3>Our Latest Work</h3>
-		    			<div class="portfolio-item">
-							<div class="portfolio-image">
-								<a href="page-portfolio-item.html"><img src="img/portfolio6.jpg" alt="Project Name"></a>
-							</div>
-						</div>
-		    		</div>
-		    		<div class="col-footer col-md-3 col-xs-6">
-		    			<h3>Navigate</h3>
-		    			<ul class="no-list-style footer-navigate-section">
-		    				<li><a href="page-blog-posts.html">Blog</a></li>
-		    				<li><a href="page-portfolio-3-columns-2.html">Portfolio</a></li>
-		    				<li><a href="page-products-3-columns.html">eShop</a></li>
-		    				<li><a href="page-services-3-columns.html">Services</a></li>
-		    				<li><a href="page-pricing.html">Pricing</a></li>
-		    				<li><a href="page-faq.html">FAQ</a></li>
-		    			</ul>
-		    		</div>
+
+
 		    		
 		    		<div class="col-footer col-md-4 col-xs-6">
 		    			<h3>Contacts</h3>
 		    			<p class="contact-us-details">
-	        				<b>Address:</b> 123 Fake Street, LN1 2ST, London, United Kingdom<br/>
+	        				<b>Address:</b> Hogeschoollaan 1, Breda<br/>
 	        				<b>Phone:</b> +44 123 654321<br/>
 	        				<b>Fax:</b> +44 123 654321<br/>
-	        				<b>Email:</b> <a href="mailto:getintoutch@yourcompanydomain.com">getintoutch@yourcompanydomain.com</a>
+	        				<b>Email:</b> <a href="mailto:djamleij@avans.nl">Bookstore@avans.nl</a>
 	        			</p>
 		    		</div>
 		    	</div>
--- pages/index.php
+++ pages/index.php
@@ -5,31 +5,31 @@
 					<!-- Slide 1 -->
 					<li class="bg4">
 						<!-- Slide Title -->
-						<h2 class="title">Responsive</h2>
+						<h2 class="title">Bookstore</h2>
 						<!-- Slide Text -->
-						<h3 class="subtitle">It looks great on desktops, laptops, tablets and smartphones</h3>
+						<h3 class="subtitle">BOEKEN, BOEKEN EN NOG MEER BOEKEN!</h3>
 						<!-- Slide Image -->
-						<img class="slide-img" src="img/homepage-slider/slide1.png" alt="Slide 1" />
+						<img class="slide-img" src="layout/img/homepage-slider/slide4.png" alt="Slide 1" />
 					</li>
 					<!-- End Slide 1 -->
 					<!-- Slide 2 -->
 					<li class="bg3">
 						<!-- Slide Title -->
-						<h2 class="title">Color Schemes</h2>
+						<h2 class="title">Stripboeken</h2>
 						<!-- Slide Text -->
-						<h3 class="subtitle">Comes with 5 color schemes and it's easy to make your own!</h3>
+						<h3 class="subtitle">Batman, Superman, Spiderman.. noem maar op!</h3>
 						<!-- Slide Image -->
-						<img class="slide-img" src="img/homepage-slider/slide2.png" alt="Slide 2" />
+						<img class="slide-img" src="layout/img/homepage-slider/slide5.png" alt="Slide 2" />
 					</li>
 					<!-- End Slide 2 -->
 					<!-- Slide 3 -->
 					<li class="bg1">
 						<!-- Slide Title -->
-						<h2 class="title">Feature Rich</h2>
+						<h2 class="title">Manga</h2>
 						<!-- Slide Text -->
-						<h3 class="subtitle">Huge amount of components and over 30 sample pages!</h3>
+						<h3 class="subtitle">Tot wel 10000 verschillende manga's!</h3>
 						<!-- Slide Image -->
-						<img class="slide-img" src="img/homepage-slider/slide3.png" alt="Slide 3" />
+						<img class="slide-img" src="layout/img/homepage-slider/slide6.png" alt="Slide 3" />
 					</li>
 					<!-- End Slide 3 -->
 				</ul>
@@ -50,17 +50,17 @@
 				<div class="row">
 					<div class="col-md-4 col-sm-6">
 						<div class="in-press press-wired">
-							<a href="#">Morbi eleifend congue elit nec sagittis. Praesent aliquam lobortis tellus, nec consequat vitae</a>
+							<a href="#">Wij hebben alleen maar lof voor Bookstore, de 24/7 klantenservice is perfect!</a>
 						</div>
 					</div>
 					<div class="col-md-4 col-sm-6">
 						<div class="in-press press-mashable">
-							<a href="#">Morbi eleifend congue elit nec sagittis. Praesent aliquam lobortis tellus, nec consequat vitae</a>
+							<a href="#">Wij bestellen bij Bookstore en we doneren de boeken aan kindern in Timboektoe, ze zijn er werkelijk waar weg van!</a>
 						</div>
 					</div>
 					<div class="col-md-4 col-sm-6">
 						<div class="in-press press-techcrunch">
-							<a href="#">Morbi eleifend congue elit nec sagittis. Praesent aliquam lobortis tellus, nec consequat vitae</a>
+							<a href="#">Top in elkaar gezet, kan er geen woordje aan vuil maken</a>
 						</div>
 					</div>
 				</div>
@@ -68,52 +68,8 @@
 		</div>
 		<!-- Press Coverage -->
 
-		<!-- Services -->
-        <div class="section">
-	        <div class="container">
-	        	<div class="row">
-	        		<div class="col-md-4 col-sm-6">
-	        			<div class="service-wrapper">
-		        			<img src="img/service-icon/diamond.png" alt="Service 1">
-		        			<h3>Aliquam in adipiscing</h3>
-		        			<p>Praesent rhoncus mauris ac sollicitudin vehicula. Nam fringilla turpis turpis, at posuere turpis aliquet sit amet condimentum</p>
-		        			<a href="#" class="btn">Read more</a>
-		        		</div>
-	        		</div>
-	        		<div class="col-md-4 col-sm-6">
-	        			<div class="service-wrapper">
-		        			<img src="img/service-icon/ruler.png" alt="Service 2">
-		        			<h3>Curabitur mollis</h3>
-		        			<p>Suspendisse eget libero mi. Fusce ligula orci, vulputate nec elit ultrices, ornare faucibus orci. Aenean lectus sapien, vehicula</p>
-		        			<a href="#" class="btn">Read more</a>
-		        		</div>
-	        		</div>
-	        		<div class="col-md-4 col-sm-6">
-	        			<div class="service-wrapper">
-		        			<img src="img/service-icon/box.png" alt="Service 3">
-		        			<h3>Vivamus mattis</h3>
-		        			<p>Phasellus posuere et nisl ac commodo. Nulla facilisi. Sed tincidunt bibendum cursus. Aenean vulputate aliquam risus rutrum scelerisque</p>
-		        			<a href="#" class="btn">Read more</a>
-		        		</div>
-	        		</div>
-	        	</div>
-	        </div>
-	    </div>
-	    <!-- End Services -->
 
-		<!-- Call to Action Bar -->
-	    <div class="section section-white">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<div class="calltoaction-wrapper">
-							<h3>It's a free multipurpose Bootstrap 3 template!</h3> <a href="http://www.dragdropsite.com" class="btn btn-orange">Download here!</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- End Call to Action Bar -->
+
 
 		<!-- Testimonials -->
 	    <div class="section">
@@ -124,17 +80,17 @@
 					<div class="testimonial col-md-4 col-sm-6">
 						<!-- Author Photo -->
 						<div class="author-photo">
-							<img src="img/user1.jpg" alt="Author 1">
+							<img src="layout/img/user1.jpg" alt="Author 1">
 						</div>
 						<div class="testimonial-bubble">
 							<blockquote>
 								<!-- Quote -->
 								<p class="quote">
-		                            "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut."
+		                            "Ik ben super blij met de snelle service van Bookstore, binnen 24 uur was mijn bestelling in huis!"
                         		</p>
                         		<!-- Author Info -->
                         		<cite class="author-info">
-                        			- Name Surname,<br>Managing Director at <a href="#">Some Company</a>
+                        			- Sjonnie Batsbak,<br>Bestelde de reeks van Harry Potter
                         		</cite>
                         	</blockquote>
                         	<div class="sprite arrow-speech-bubble"></div>
@@ -143,15 +99,15 @@
                     <!-- End Testimonial -->
                     <div class="testimonial col-md-4 col-sm-6">
 						<div class="author-photo">
-							<img src="img/user5.jpg" alt="Author 2">
+							<img src="layout/img/user5.jpg" alt="Author 2">
 						</div>
 						<div class="testimonial-bubble">
 							<blockquote>
 								<p class="quote">
-		                            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo."
+		                            "Door Bookstore ben ik elke week weer voorzien van een nieuw, spannend boek. Door de persoonlijke service en de snelle bezorging!"
                         		</p>
                         		<cite class="author-info">
-                        			- Name Surname,<br>Managing Director at <a href="#">Some Company</a>
+                        			- Sieneke van Breda,<br>Vaste klant bij Bookstore
                         		</cite>
                         	</blockquote>
                         	<div class="sprite arrow-speech-bubble"></div>
@@ -159,15 +115,15 @@
                     </div>
 					<div class="testimonial col-md-4 col-sm-6">
 						<div class="author-photo">
-							<img src="img/user2.jpg" alt="Author 3">
+							<img src="layout/img/user2.jpg" alt="Author 3">
 						</div>
 						<div class="testimonial-bubble">
 							<blockquote>
 								<p class="quote">
-		                            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+		                            "Ik hou heel erg van Strip(pen)boeken, hentai, manga het maakt me niet uit. Bookstore heeft het!"
                         		</p>
                         		<cite class="author-info">
-                        			- Name Surname,<br>Managing Director at <a href="#">Some Company</a>
+                        			- Cha-Nay-Nay de Vries,<br>Houdt van Hentai en Manga
                         		</cite>
                         	</blockquote>
                         	<div class="sprite arrow-speech-bubble"></div>
@@ -178,89 +134,4 @@
 	    </div>
 	    <!-- End Testimonials -->
 
-		<!-- Pricing Table -->
-	    <div class="section">
-	    	<div class="container">
-	    		<h2>Pricing</h2>
-	        	<div class="row">
-	        		<!-- Pricing Plans Wrapper -->
-	        		<div class="pricing-wrapper col-md-12">
-        				<!-- Pricing Plan -->
-						<div class="pricing-plan">
-							<!-- Pricing Plan Ribbon -->
-							<div class="ribbon-wrapper">
-								<div class="price-ribbon ribbon-red">Popular</div>
-							</div>
-							<h2 class="pricing-plan-title">Starter</h2>
-							<p class="pricing-plan-price">FREE</p>
-							<!-- Pricing Plan Features -->
-							<ul class="pricing-plan-features">
-								<li><strong>1</strong> user</li>
-								<li><strong>Unlimited</strong> projects</li>
-								<li><strong>2GB</strong> storage</li>
-							</ul>
-							<a href="index.html" class="btn">Order Now</a>
-						</div>
-						<!-- End Pricing Plan -->
-					    <div class="pricing-plan">
-							<h2 class="pricing-plan-title">Advanced</h2>
-							<p class="pricing-plan-price">$49<span>/mo</span></p>
-								<ul class="pricing-plan-features">
-									<li><strong>10</strong> users</li>
-									<li><strong>Unlimited</strong> projects</li>
-									<li><strong>20GB</strong> storage</li>
-								</ul>
-							<a href="index.html" class="btn">Order Now</a>
-					    </div>
-					    <!-- Promoted Pricing Plan -->
-					    <div class="pricing-plan pricing-plan-promote">
-								<h2 class="pricing-plan-title">Premium</h2>
-								<p class="pricing-plan-price">$99<span>/mo</span></p>
-								<ul class="pricing-plan-features">
-									<li><strong>Unlimited</strong> users</li>
-									<li><strong>Unlimited</strong> projects</li>
-									<li><strong>100GB</strong> storage</li>
-								</ul>
-							<a href="index.html" class="btn">Order Now</a>
-					    </div>
-					    <div class="pricing-plan">
-					    	<!-- Pricing Plan Ribbon -->
-							<div class="ribbon-wrapper">
-								<div class="price-ribbon ribbon-green">New</div>
-							</div>
-							<h2 class="pricing-plan-title">Mega</h2>
-							<p class="pricing-plan-price">$199<span>/mo</span></p>
-								<ul class="pricing-plan-features">
-									<li><strong>Unlimited</strong> users</li>
-									<li><strong>Unlimited</strong> projects</li>
-									<li><strong>100GB</strong> storage</li>
-								</ul>
-							<a href="index.html" class="btn">Order Now</a>
-					    </div>
-	        		</div>
-	        		<!-- End Pricing Plans Wrapper -->
-	        	</div>
-	    	</div>
-	    </div>
-	    <!-- End Pricing Table -->
-
-		<!-- Our Clients -->
-	    <div class="section">
-	    	<div class="container">
-	    		<h2>Our Clients</h2>
-				<div class="clients-logo-wrapper text-center row">
-					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="img/logos/canon.png" alt="Client Name"></a></div>
-					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="img/logos/cisco.png" alt="Client Name"></a></div>
-					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="img/logos/dell.png" alt="Client Name"></a></div>
-					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="img/logos/ea.png" alt="Client Name"></a></div>
-					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="img/logos/ebay.png" alt="Client Name"></a></div>
-					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="img/logos/facebook.png" alt="Client Name"></a></div>
-					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="img/logos/google.png" alt="Client Name"></a></div>
-					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="img/logos/hp.png" alt="Client Name"></a></div>
-					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="img/logos/microsoft.png" alt="Client Name"></a></div>
-					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="img/logos/mysql.png" alt="Client Name"></a></div>
-					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="img/logos/sony.png" alt="Client Name"></a></div>
-					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="img/logos/yahoo.png" alt="Client Name"></a></div>
-				</div>
-			</div>
-	    </div>
+		
