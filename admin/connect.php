25066e50f640b53ebb0855488f2202b6763b6b2a
Samir <samo.dn@gmail.com>
2017-06-21 11:35:54 +0200

    delete db password

--- admin/connect.php
+++ admin/connect.php
@@ -2,7 +2,7 @@
  // Database connection
  $dsn 		= 'mysql:host=localhost;dbname=books_db';
  $username	= 'root';
- $password	= 'myadminpassword321@#';
+ $password	= '';
  $option = array(
  			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
  	);
