48758c3d273a7495b079645e50ec50eb26e02f08
dylanleijten <djamleij@avans.nl>
2017-06-22 11:43:11 +0200

    contact page

---
+++ pages/contact.php
@@ -0,0 +1,91 @@
+<!-- Page Title -->
+		<div class="section section-breadcrumbs">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<h1>Contact</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+        
+        <div class="section">
+	    	<div class="container">
+	        	<div class="row">
+	        		<div class="col-sm-7">
+	        			<!-- Map -->
+	        			<div id="contact-us-map">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2479.1392307074293!2d4.795066216104124!3d51.58401157964791!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6a1d7942a1a21%3A0x46b0efbbbd6471c7!2sAvans+Hogeschool+-+Locatie+Hogeschoollaan!5e0!3m2!1snl!2snl!4v1498123805322" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+	        			</div>
+	        			<!-- End Map -->
+	        			<!-- Contact Info -->
+	        			<p class="contact-us-details">
+	        				<b>Address:</b> Hogeschoollaan 1, Breda<br/>
+	        				<b>Phone:</b> +44 123 654321<br/>
+	        				<b>Fax:</b> +44 123 654321<br/>
+	        				<b>Email:</b> <a href="mailto:Bookstore@avans.nl">Bookstore@avans.nl</a>
+	        			</p>
+	        			<!-- End Contact Info -->
+	        		</div>
+	        		<div class="col-sm-5">
+	        			<!-- Contact Form -->
+	        			<h3>Stuur ons een bericht!</h3>
+	        			<div class="contact-form-wrapper">
+		        			<form class="form-horizontal" role="form">
+		        				 <div class="form-group">
+		        				 	<label for="Name" class="col-sm-3 control-label"><b>Naam</b></label>
+		        				 	<div class="col-sm-9">
+										<input class="form-control" id="Name" type="text" placeholder="">
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="contact-email" class="col-sm-3 control-label"><b>Email</b></label>
+									<div class="col-sm-9">
+										<input class="form-control" id="contact-email" type="text" placeholder="">
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="contact-message" class="col-sm-3 control-label"><b>Selecteer onderwerp</b></label>
+									<div class="col-sm-9">
+										<select class="form-control" id="prependedInput">
+											<option>Selecteer het onderwerp</option>
+											<option>Algemeen</option>
+											<option>Services</option>
+											<option>Orders</option>
+										</select>
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="contact-message" class="col-sm-3 control-label"><b>Bericht</b></label>
+									<div class="col-sm-9">
+										<textarea class="form-control" rows="5" id="contact-message"></textarea>
+									</div>
+								</div>
+								<div class="form-group">
+									<div class="col-sm-12">
+										<button type="submit" class="btn pull-right">Send</button>
+									</div>
+								</div>
+		        			</form>
+		        		</div>
+		        		<!-- End Contact Info -->
+	        		</div>
+	        	</div>
+	    	</div>
+	    </div>
+
+	    
+
+        <!-- Javascripts -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/jquery-1.9.1.min.js"><\/script>')</script>
+        <script src="js/bootstrap.min.js"></script>
+
+        <script src="js/jquery.fitvids.js"></script>
+        <script src="js/jquery.sequence-min.js"></script>
+        <script src="js/jquery.bxslider.js"></script>
+        <script src="js/main-menu.js"></script>
+        <script src="js/template.js"></script>
+
+    </body>
+</html>
